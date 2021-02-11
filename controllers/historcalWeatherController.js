357e2318dbdf34ce10043439149cca7658159427
Aditya <adi..@Adityas-MacBook-Air.local>
2021-02-11 18:38:47 +0000

    test

--- controllers/historcalWeatherController.js
+++ controllers/historcalWeatherController.js
@@ -4,6 +4,7 @@ const User = require('../models/user_structure');
 var now = new Date();
 var startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
 var timestamp = startOfDay / 1000;
+const schedule = require ('node-schedule');
 
 let url = "http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=53.34399&lon=-6.26719&dt="+timestamp+"&appid=6479fa080a30d358d9e38b388fc8c697";
 let settings = { method: "Get" };
@@ -13,8 +14,37 @@ fetch(url, settings)
     .then((json) => {
         JSON=json
     });
+    
 
-
+schedule.scheduleJob('0 0 * * *', () => { 
+    History.findOne({ 'data.current.dt': timestamp },async function (err, his) {
+        if (err) {
+            console.log('error', 'Weather Data Creation failed : ', err.toString());
+            res.send({ success: false, message: 'Something bad happend, please try again' });
+            return;
+        }
+        else if (his == null) {
+            var x={date:startOfDay,data:JSON}
+            History.create(x,function(err,data){
+                if (err){
+                         console.log('error','User Create failed',user); 
+                         res.json({success: false, message: 'User Creation failed failed'});
+                         return;
+                       }
+                console.log('info','Data For timestamp '+timestamp+' Created');
+                res.json({'success':true,'message':'Data For timestamp '+timestamp+' Created','data':JSON});
+                return;
+            });
+        } 
+        else {
+            console.log('info','Timestamp '+timestamp+' is already Created');
+            return res.json({
+                success:false,
+                message: "Already available for the timestamp: "+his.data.current.dt
+            })
+        }
+    });
+})
 
 
 exports.getHistory = async (req, res) => {
@@ -44,6 +74,9 @@ exports.getHistory = async (req, res) => {
     });
 
   };
+
+
+
 
   exports.enterHistory = async (req, res) => {
 
