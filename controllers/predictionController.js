ad8fe717ab47094cfec466def7e39c32d778a7a0
Aditya <adi..@Adityas-MacBook-Air.local>
2021-02-17 12:04:29 +0000

    dfwe

--- controllers/predictionController.js
+++ controllers/predictionController.js
@@ -4,8 +4,9 @@ const fetch = require('node-fetch');
 let url = "http://3.82.248.105/get-humidity-predictions";
 let url2="http://3.82.248.105/get-wind-predictions";
 let url3="http://3.82.248.105/get-cloudy-predictions";
+let url4 = "http://3.82.248.105/get-temp-predictions";
 let settings = { method: "Get" };
-let JSON,JSON2,JSON3
+let JSON,JSON2,JSON3,JSON4
 fetch(url, settings)
     .then(res => res.json())
     .then((json) => {
@@ -25,9 +26,15 @@ fetch(url3, settings)
     JSON3=json
 });
 
+fetch(url4, settings)
+.then(res => res.json())
+.then((json) => {
+    JSON4=json
+});
 
 exports.getPrediction = async (req, res) => {
     return res.json({humidityPrediction:JSON,
     windPrediction:JSON2,
-    cloudyPrediction:JSON3});
+    cloudyPrediction:JSON3,
+    tempPrediction:JSON4});
   };
