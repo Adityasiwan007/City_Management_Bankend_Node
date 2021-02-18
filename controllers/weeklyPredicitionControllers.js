b6d0f38600130c0c0f1d9b3c67928a7861d74673
Aditya <adi..@Adityas-MacBook-Air.local>
2021-02-18 11:03:04 +0000

    test

--- controllers/weeklyPredicitionControllers.js
+++ controllers/weeklyPredicitionControllers.js
@@ -5,10 +5,11 @@ let url ="http://3.82.248.105/get-temp-weekly";
 let url2="http://3.82.248.105/get-rain-weekly";
 let url3="http://3.82.248.105/get-humidity-weekly";
 let url4="http://3.82.248.105/get-cloud-weekly";
+let url5="http://3.82.248.105/get-wind-weekly";
 let settings = { method: "Get" };
-let JSON,JSON2,JSON3,JSON4
-let cloud=['Sunny','Partially Cloudy','Clody','Raining','Sunny'],icon=['w01d','w03d','w09d','w13d','w50d'],i
-
+let JSON,JSON2,JSON3,JSON4,JSON5;
+//let cloud=['Sunny','Partially Cloudy','Clody','Raining','Sunny'],icon=['w01d','w03d','w09d','w13d','w50d'],i
+let cloud=[],icon=[]
 fetch(url, settings)
     .then(res => res.json())
     .then((json) => {
@@ -31,11 +32,36 @@ fetch(url4, settings)
 .then(res => res.json())
 .then((json) => {
     JSON4=json
-    // for(i=0;i<5;i++)
-    // {
-    //     cloud.push(JSON4[i])
-    // }
-    // console.log(cloud);
+    for(i=0;i<5;i++)
+    {
+        if(JSON4[i]<=30)
+        {
+            cloud.push('Sunny')
+            icon.push('w01d')
+        }
+        else if(JSON[i]>30 && JSON[i]<=50)
+        {
+            cloud.push('Partially Cloudy')
+            icon.push('w02d')
+        }
+        else if(JSON4[i]>50 && JSON4[i]<=80)
+        {
+            cloud.push('Clody')
+            icon.push('w04d')
+        }
+        else
+        {
+            cloud.push('Raining')
+            icon.push('w09d')
+        }
+    }
+    console.log(cloud);
+});
+
+fetch(url5, settings)
+.then(res => res.json())
+.then((json) => {
+    JSON5=json
 });
 
 
@@ -45,6 +71,7 @@ exports.getWeekly = async (req, res) => {
     rainWeekly:JSON2,
     humidityWeekly:JSON3,
     cloudWeekly:JSON4,
+    windWeekly:JSON5,
     cloudString:cloud,
     icon:icon});
   };
